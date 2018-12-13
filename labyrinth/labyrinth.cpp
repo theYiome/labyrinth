9964e288bcbf34980c559fec23108592eabd3c13
theYiome <kamil.pasterczyk@gmail.com>
2018-12-13 12:26:37 +0100

    empty project initialized with SFML and GLEW

---
+++ labyrinth/labyrinth.cpp
@@ -0,0 +1,70 @@
+// labyrinth.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <glew.h>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+int main()
+{
+	// create the window
+	sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+	window.setVerticalSyncEnabled(true);
+
+	// GLEW initialization
+	glewInit();
+	if (glewInit() != GLEW_OK) return 911;
+
+	// activate the window
+	window.setActive(true);
+
+	// load resources, initialize the OpenGL states, ...
+
+	// run the main loop
+	bool running = true;
+	while (running)
+	{
+		// handle events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				// end the program
+				running = false;
+			}
+			else if (event.type == sf::Event::Resized)
+			{
+				// adjust the viewport when the window is resized
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+		}
+
+		// clear the buffers
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// draw...
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
+		// end the current frame (internally swaps the front and back buffers)
+		window.display();
+	}
+
+	// release resources...
+
+	return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
